1b130ba4731341b8d0ab02f344eb5ed39eaa4780
gaega nooson <gaega@gaegas-MacBook-Air.local>
2025-12-30 19:49:39 -0600

    First code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <div className="todo-box">
+        <h1 className="todo-title">To-Do list</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
