b47a24e06ba5dab9b6bcb53630228144110417c4
gaega nooson <gaega@gaegas-MacBook-Air.local>
2025-12-30 20:24:01 -0600

    Search bar added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ const App = () => {
     <div className="App">
       <div className="todo-box">
         <h1 className="todo-title">To-Do list</h1>
+
+        <div className="todo-input-row">
+          <input
+            type="text"
+            placeholder="Add a new task..."
+            className="todo-input"
+          />
+          <button className="todo-button">Add</button>
+        </div>
       </div>
     </div>
   );
